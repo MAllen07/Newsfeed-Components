a0f317ecedf417674eaf870f2c5009c69f6e5546
MAllen07 <mrsmallen07@gmail.com>
2019-03-13 21:23:56 -0400

    finish article js file also error found with less compiler crashed

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,20 +1,7 @@
-
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
-
-  menu.classlist.toggle('menu--open');
+  menu.classList.toggle('menu--open')
 }
 
-// Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector ('.menu');
-console.log (menu);
-
-const menu;
-// create a reference to the ".menu-button" class
-const menuButton = document.querySelector ('menu-button');
-
-const menuButton;
-// Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener ('click', toggleMenu);
-
-menu.forEach= (menu => new menu(menu));
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', toggleMenu);
